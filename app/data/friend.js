6715f4b74421bbbca2c5659945106072bc8a3358
kpoole133 <kpoole133@gmail.com>
2019-09-28 08:51:58 -0400

    Friend Finder Application

---
+++ app/data/friend.js
@@ -0,0 +1,20 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'Friend1',
+      profilePic: '',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
